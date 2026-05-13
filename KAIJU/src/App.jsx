cffc552bf7b18ee80b7d9b2fe425720c86ad8c58
Elite <kimo123kimo321@gmail.com>
2026-05-13 12:04:42 +0300

    studying

--- KAIJU/src/App.jsx
+++ KAIJU/src/App.jsx
@@ -5,7 +5,6 @@ import GenresSection from "./components/GenresSection";
 import Footer from "./components/Footer";
 
 export default function App() {
-  console.log("spacing");
   
   return (
     <div className="bg-zinc-950">
