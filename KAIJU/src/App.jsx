9641863dc5ca8b64a7a34bd1ca9d1121ac51a7bb
Elite <kimo123kimo321@gmail.com>
2026-05-28 04:27:42 +0300

    fail

--- KAIJU/src/App.jsx
+++ KAIJU/src/App.jsx
@@ -9,7 +9,7 @@ import Anime from "./pages/Anime";
 import Manga from "./pages/Manga";
 
 export default function App() {
-  console.log("i'll study afternoon or i'll take a break ");
+  console.log("4 days omg im fucking disappointed");
   
   
   const [topAnime, setTopAnime] = useState([]);
